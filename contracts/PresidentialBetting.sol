9bff1d1d0ce751c1fbdb7e0adfcd1ae69cdbcead
Eli Bradley <elibradley@utexas.edu>
2022-02-15 15:58:46 -0600

    betting contract

--- contracts/PresidentialBetting.sol
+++ contracts/PresidentialBetting.sol
@@ -2,11 +2,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Commodity is ERC20 {
     address owner;
 
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+    constructor(string memory name) ERC20(name, name) {
         owner = msg.sender;
     }
 
@@ -28,13 +29,13 @@ enum Candidate {
 }
 
 contract PredictionMarket {
-    address[] arbiters;
-    mapping (address => Candidate) votes;
+    address[] public arbiters;
+    mapping (address => Candidate) public votes;
     mapping (Candidate => Commodity) commodities;
 
     constructor(address[] memory arbiters_) {
-        commodities[Candidate.Trump] = new Commodity("Trump", "TRUMP");
-        commodities[Candidate.Biden] = new Commodity("Biden", "BIDEN");
+        commodities[Candidate.Trump] = new Commodity("TRUMP");
+        commodities[Candidate.Biden] = new Commodity("BIDEN");
         arbiters = arbiters_;
     }
 
@@ -54,20 +55,21 @@ contract PredictionMarket {
             voteCounts[uint(votes[arbiters[i]])]++;
         }
 
-        uint max;
-        for (uint i = 1; i < 3; i++) {
-            if (voteCounts[i] >= voteCounts[max]) {
-                max = i;
+        for (uint i = 0; i < 3; i++) {
+            if (3 * voteCounts[i] >= 2 * arbiters.length) {
+                // Supermajority vote.
+                return Candidate(i);
             }
         }
 
-        return Candidate(max);
+        return Candidate.None;
     }
 
     function payout(address payable recipient) public {
         Candidate winner = getWinner();
-        uint winnerValue = commodities[winner].totalSupply();
+        require(winner != Candidate.None);
 
+        uint winnerValue = commodities[winner].totalSupply();
         uint totalValue = 0;
         for (uint i = 1; i < 3; i++) {
             totalValue += commodities[Candidate(i)].totalSupply();
