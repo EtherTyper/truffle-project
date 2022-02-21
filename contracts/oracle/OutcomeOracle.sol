b47f6190a164af21e3a61ceb14db9a9736beb637
Eli Bradley <elibradley@utexas.edu>
2022-02-21 15:00:05 -0600

    Generalize outcome oracle contract.

--- contracts/oracle/OutcomeOracle.sol
+++ contracts/oracle/OutcomeOracle.sol
@@ -5,17 +5,11 @@ pragma solidity ^0.8.12;
 interface OutcomeOracle {
     function numOutcomes() external returns (uint256);
 
-    function arbiters(uint256) external returns (address);
-
-    function votes(address) external returns (uint256);
-
     function outcomes(uint256) external returns (string memory);
 
     function decided() external returns (bool);
 
     function decision() external returns (uint256);
-
-    function vote(uint256 outcome) external;
 
     function recordWinner() external;
 }
