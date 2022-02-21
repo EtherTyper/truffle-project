9c2adff74f3e281ad9e92b4c71f76106392fbdd4
Eli Bradley <elibradley@utexas.edu>
2022-02-21 15:07:06 -0600

    Start combinatorial oracle

--- contracts/oracle/CombinatorialOracle.sol
+++ contracts/oracle/CombinatorialOracle.sol
@@ -4,6 +4,13 @@ pragma solidity ^0.8.12;
 
 import "./OutcomeOracle.sol";
 
-abstract contract CombinatorialOracle is OutcomeOracle {
+// Outcomes will be CSV strings of suboracle outcomes.
 
+abstract contract CombinatorialOracle is OutcomeOracle {
+    OutcomeOracle[] public suboracles;
+    uint256 public numOutcomes;
+
+    constructor(OutcomeOracle[] memory suboracles_) {
+        suboracles = suboracles_;
+    }
 }
